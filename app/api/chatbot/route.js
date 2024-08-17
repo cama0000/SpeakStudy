f5f601b66f083d5f265689045b6ee25197efc8a9
cama0000 <acama0000@gmail.com>
2024-08-17 14:58:54 -0500

    revised gemini call

--- app/api/chatbot/route.js
+++ app/api/chatbot/route.js
@@ -36,57 +36,30 @@
 //   }
 // }
 
-import { NextResponse } from "next/server";
+
+
+
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function POST(req) {
-  // set up gemini and get user message
+    const { filePath, question } = await req.json();
+    console.log("filePath and question: " + filePath + " " + question);
     const apiKey = process.env.GEMINI_API_KEY;
     const genAI = new GoogleGenerativeAI(apiKey);
-    const data = await req.json();
-    const userMessage = data.message;
-    const context = data.context; // ???? pass in text of pdf as well
 
-    const systemPrompt = `You are an educational assistant. Users will pass in their lectures and they will ask questions about it.
-    You must provide a response to the user's question based on the lecture content.  
+    // const systemPrompt = `You are an educational assistant. Users will pass in their lectures and they will ask questions about it.
+    // You must provide a response to the user's question based on the lecture content.  
 
-    CONTEXT: ${context}
+    // CONTEXT: ${context}
 
-    USER: ${userMessage}`;
+    // USER: ${userMessage}`;
 
     const model = genAI.getGenerativeModel({
         model: "gemini-1.0-pro",
     });
 
     const chat = model.startChat()
-    let result = await model.generateContent(systemPrompt)
+    let result = await model.generateContent(question)
 
-    // make sure this returns text idkkk
-    return NextResponse.json({ text: result.response.text() });
-
-    // THIS IS JUST FOR STREAMING RESPONSE
-    // const controller = new AbortController();
-    // const signal = controller.signal;
-
-    // const stream = new ReadableStream({
-    //     async start(controller) {
-    //         try {
-    //             const result = await model.generateContentStream(conversation, { signal });
-
-    //             const encoder = new TextEncoder();
-
-    //             for await (const chunk of result.stream) {
-    //                 const chunkText = chunk.text();
-    //                 controller.enqueue(encoder.encode(chunkText));
-    //             }
-
-    //             controller.close();
-    //         } catch (error) {
-    //             console.error('Error generating content:', error);
-    //             controller.error(error);
-    //         }
-    //     },
-    // });
-
-    // return new NextResponse(stream);
+    return new Response(JSON.stringify({ reply: result.response.text() }), { status: 200 });
 }
